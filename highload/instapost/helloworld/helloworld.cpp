b8f7b767a5ab53b6da6af1454ae1fa6c4b9cdd59
crossby <valery.duzhik@outlook.com>
2016-11-22 14:01:13 +0300

    highload added

---
+++ highload/instapost/helloworld/helloworld.cpp
@@ -0,0 +1,35 @@
+#include <fastcgi2/component.h>
+#include <fastcgi2/component_factory.h>
+#include <fastcgi2/handler.h>
+#include <fastcgi2/request.h>
+
+#include <iostream>
+#include <sstream>
+
+class HelloWorldClass : virtual public fastcgi::Component, virtual public fastcgi::Handler {
+
+public:
+        HelloWorldClass(fastcgi::ComponentContext *context) :
+                fastcgi::Component(context) {
+        }
+        virtual ~HelloWorldClass() {
+        }
+
+public:
+        virtual void onLoad() {
+        }
+        virtual void onUnload() {
+        }
+        virtual void handleRequest(fastcgi::Request *request, fastcgi::HandlerContext *context) {
+
+                request->setContentType("text/plain");
+                std::stringbuf buffer("Hello " + (request->hasArg("name") ? request->getArg("name") : "stranger\n"));
+                request->write(&buffer);
+
+        }
+
+};
+
+FCGIDAEMON_REGISTER_FACTORIES_BEGIN()
+FCGIDAEMON_ADD_DEFAULT_FACTORY("simple_factory", HelloWorldClass)
+FCGIDAEMON_REGISTER_FACTORIES_END()
